c365fde5c19abc4a20175228e6816c454e650a92
Zach Baskin <zbaskin@gatech.edu>
2020-06-23 11:19:13 -0400

    fixed algorithm to work and added error handling

--- Analyzer.java
+++ Analyzer.java
@@ -1,6 +1,6 @@
 class Analyzer {
 
-  static final int INFINITY = 1000000;
+  //static final int INFINITY = 1000000;
 
   public static int min(int x, int y) {
     if (x < y) {
@@ -9,28 +9,45 @@ class Analyzer {
     return y;
   }
 
-  public static int change(int denominations[], int make, int numberOf) {
-    int[] temp = new int[make + 1];
-    temp[0] = 0;
+  public static int change(int denominations[], int n) {
+    int[] temp = new int[n + 1];
+    temp[0] = 1;
 
-    for (int i = 1; i <= make; i++) {
-      int minimum = INFINITY;
+    for (int i = 0; i < denominations.length; i++) {
+      //int minimum = INFINITY;
 
-      for (int j = 0; j <= numberOf; j++) {
-        if (i >= denominations[j]) {
-          minimum = min(minimum, 1 + temp[i - denominations[j]]);
+      for (int j = 0; j < temp.length; j++) {
+        if ( /*(j < denominations.length) &&*/ (j >= denominations[i]) ) {
+          temp[j] += temp[j - denominations[i]];
         }
       }
 
-      temp[i] = minimum;
+      //temp[i] = minimum;
     }
-    return temp[make];
+    return temp[n];
+  }
+
+  static void printArray(int denominations[]) {
+    System.out.print("[ " + denominations[0]);
+    for (int i = 1; i < denominations.length; i++) {
+      System.out.print(", ");
+      System.out.print(denominations[i]);
+    }
+    System.out.println(" ]");
   }
 
   public static void main(String[] args) {
-    int denoms[] = {0, 1, 2, 3};
-    int n = Integer.parseInt(args[0]);
-    int k = Integer.parseInt(args[1]);
-    System.out.println(change( denoms, n, k ));
+    if (args.length != 1) {
+      System.out.println("Please include one number.");
+    } else {
+      int denoms[] = {1, 5, 10};
+      int n = Integer.parseInt(args[0]);
+      //int k = Integer.parseInt(args[1]);
+      System.out.print("The Coins Array: ");
+      printArray(denoms);
+
+      System.out.print("Solution: ");
+      System.out.println(change( denoms, n ));
+    }
   }
 }
